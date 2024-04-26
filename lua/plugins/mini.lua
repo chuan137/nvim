8db05e1f21687d22475c8fb8e4c2a7fbf855356b
chuan137 <chuan137@gmail.com>
2024-04-26 23:51:22 +0200

    add mini plugins

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,13 +1,29 @@
 return {
   {
-    'echasnovski/mini.bracketed',
+    'echasnovski/mini.nvim',
     event = 'VeryLazy',
-  },
-
-  {
-    'echasnovski/mini.comment',
-    version = '*',
-    event = 'VeryLazy',
-    opts = {},
+    keys = {
+      {'<leader>e', '<cmd>lua MiniFiles.open()<cr>', 'Open Mini Explorer'},
+    },
+    opts = {
+      basics = {
+        autocommands = {
+          relnum_in_visual_mode = true,
+        },
+      },
+      bracketed = {
+        file = { suffix = '' },
+      },
+    },
+    config = function(_, opts)
+      require('mini.ai').setup(opts.ai or {})
+      require('mini.basics').setup(opts.basics or {})
+      require('mini.bracketed').setup(opts.bracketed or {})
+      require('mini.comment').setup(opts.comment or {})
+      require('mini.cursorword').setup(opts.cursorword or {})
+      require('mini.files').setup(opts.files or {})
+      require('mini.pairs').setup(opts.pairs or {})
+      require('mini.surround').setup(opts.surround or {})
+    end,
   },
 }
