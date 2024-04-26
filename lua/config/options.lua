d0ef4f237ff0305b747be37bcb390c12f57ecdc7
chuan137 <chuan137@gmail.com>
2024-04-27 00:28:23 +0200

    update options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,41 +2,19 @@ local fn = vim.fn
 local o = vim.o
 local opt = vim.opt
 
-opt.mouse = 'a'
-
-opt.conceallevel = 0
 opt.list = true
 opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
-opt.pumblend = 12
+opt.syntax = 'off'
+opt.swapfile = false
+opt.textwidth = 80
 
--- opt.number = true
--- opt.numberwidth = 1
--- opt.relativenumber = true
-
--- Decrease update time
+-- Decrease timeout and update time
 opt.timeout = true
 opt.timeoutlen = 300
-opt.updatetime = 250
+opt.updatetime = 500
 
 -- Movement
-opt.scrolloff = 3  -- Lines of context at the top and bottom of the screen
 opt.whichwrap = o.whichwrap .. '<,>,h,l'
-
--- Status
-opt.showmode = false
-opt.showcmd = false
-opt.showtabline = 0
-
--- Indentation
-opt.expandtab = true
-opt.smarttab = true
-opt.smartindent = true
--- opt.shiftwidth = 2
--- opt.softtabstop = 2
--- opt.tabstop = 2
-opt.linebreak = true
-opt.showbreak = string.rep(' ', 3)
-opt.breakindent = true
 
 -- Search
 opt.ignorecase = true
@@ -47,15 +25,8 @@ opt.wildmode = 'longest:full,full'
 opt.wildoptions = 'pum'
 
 -- Editing
-opt.modelines = 1
 opt.breakindent = true
 opt.showmatch = true
-opt.swapfile = false
-opt.undodir = fn.stdpath('data') .. '/undodir'
-opt.undofile = true
-opt.textwidth = 80
-opt.wrap = true
-opt.inccommand = 'split'
 opt.diffopt = {
   'internal',
   'filler',
@@ -63,9 +34,12 @@ opt.diffopt = {
   'hiddenoff',
   'algorithm:minimal',
 }
+-- opt.modelines = 1
+-- opt.inccommand = 'split'
+
 
 opt.clipboard = 'unnamedplus'
-opt.formatoptions = "jcroqlnt" -- tcqj
+opt.formatoptions = 'jcroqlnt' -- tcqj
 -- opt.formatoptions = opt.formatoptions
 --   - 't' -- wrap with text width
 --   + 'c' -- wrap comments
@@ -77,11 +51,6 @@ opt.formatoptions = "jcroqlnt" -- tcqj
 --   - '2' -- use indent of second line for paragraph
 --   + 'l' -- long lines are not broken
 --   + 'j' -- remove comment when joining lines
-opt.syntax = 'off'
-opt.spell = false
-
--- Colors
-opt.termguicolors = true
 
 -- Shada
 opt.shada = "!,'1000,f1,<1000,s100,:1000,/1000,h"
@@ -90,6 +59,16 @@ opt.shada = "!,'1000,f1,<1000,s100,:1000,/1000,h"
 -- opt.sessionoptions = "buffers,curdir,folds,help,winsize,winpos"
 
 -- UI
+opt.pumblend = 15
+opt.scrolloff = 3 -- Lines of context at the top and bottom of the screen
+opt.showtabline = 0
+opt.termguicolors = true
+
+-- wrap long lines
+opt.linebreak = true
+opt.showbreak = string.rep(' ', 3)
+opt.breakindent = true
+opt.wrap = true
 
 -- Set grep program to rg
 opt.grepprg = 'rg --vimgrep --smart-case'
