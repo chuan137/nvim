f11101bc4e3985f001a1e788f8f2dc7998783921
Chuan Miao <chuan137@gmail.com>
2025-04-18 11:59:22 +0200

    cleanup

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -14,9 +14,6 @@ map("n", "<leader>rp", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>") -
 map("v", "<leader>rp", ":s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>") -- Replace all instance of current word in file
 
 map("n", "<leader>up", "<cmd>set paste!<cr>", { desc = "Toggle Paste" })
-map("n", "<leader>ul", "<cmd>setlocal spell!<cr>", { desc = "Toggle Spell" })
-map("n", "<leader>ug", "<cmd>LazyGit<cr>", { desc = "Git" })
-
 map("n", "Q", "@q")
 
 -- diagnostic
@@ -36,18 +33,4 @@ map("n", "]e", diagnostic_goto(true, "ERROR"), { desc = "Next Error" })
 map("n", "[e", diagnostic_goto(false, "ERROR"), { desc = "Prev Error" })
 map("n", "]w", diagnostic_goto(true, "WARN"), { desc = "Next Warning" })
 map("n", "[w", diagnostic_goto(false, "WARN"), { desc = "Prev Warning" })
-
--- lsp
--- vim.api.nvim_create_autocmd("LspAttach", {
---     callback = function()
---         wk.add({ "gd", function() Snacks.picker.lsp_definitions() end, desc = "Goto Definition" })
---         wk.add({ "gD", function() Snacks.picker.lsp_declarations() end, desc = "Goto Declaration" })
---         wk.add({ "gr", function() Snacks.picker.lsp_references() end, nowait = true, desc = "References" })
---         wk.add({ "gI", function() Snacks.picker.lsp_implementations() end, desc = "Goto Implementation" })
---         wk.add({ "gy", function() Snacks.picker.lsp_type_definitions() end, desc = "Goto T[y]pe Definition" })
---         wk.add({ "<leader>ss", function() Snacks.picker.lsp_symbols() end, desc = "LSP Symbols" })
---         wk.add({ "<leader>sS", function() Snacks.picker.lsp_workspace_symbols() end, desc = "LSP Workspace Symbols" })
---         wk.add({ "<leader>cr", vim.lsp.buf.rename, desc = "Code Rename" })
---     end,
--- })
 
