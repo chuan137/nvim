641c47d01b4ab9528d4593db1b21dfe461af3ddc
Chuan Miao <chuan137@gmail.com>
2025-04-18 11:58:46 +0200

    update config/pickers

--- lua/config/pickers.lua
+++ lua/config/pickers.lua
@@ -22,22 +22,20 @@ Snacks.toggle.indent():map("<leader>ug")
 Snacks.toggle.scroll():map("<leader>uS")
 -- Snacks.toggle.profiler():map("<leader>dpp")
 -- Snacks.toggle.profiler_highlights():map("<leader>dph")
-Snacks.toggle
-    .option("conceallevel", { off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2, name = "Conceal Level" })
-    :map("<leader>uc")
-Snacks.toggle
-    .option("showtabline", { off = 0, on = vim.o.showtabline > 0 and vim.o.showtabline or 2, name = "Tabline" })
-    :map("<leader>uA")
+
+local concealLevelOpts = { off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2, name = "Conceal Level" }
+local showtablineOpts = { off = 0, on = vim.o.showtabline > 0 and vim.o.showtabline or 2, name = "Tabline" }
+Snacks.toggle.option("conceallevel", concealLevelOpts):map("<leader>uc")
+Snacks.toggle.option("showtabline", showtablineOpts):map("<leader>uA")
 
 if vim.lsp.inlay_hint then
     Snacks.toggle.inlay_hints():map("<leader>uh")
 end
 
 if vim.fn.executable("lazygit") == 1 then
-    map("n", "<leader>gg", function()
-        Snacks.lazygit({ cwd = get_git_root() })
-    end, { desc = "Lazygit (Root Dir)" })
-    map("n", "<leader>gG", "<Cmd>lua Snacks.lazygit()<CR>", { desc = "Lazygit (cwd)" })
+    -- stylua: ignore start
+    map("n", "<leader>gg", function() Snacks.lazygit({ cwd = get_git_root() }) end, { desc = "Lazygit (Root Dir)" })
+    map("n", "<leader>gg", function() Snacks.lazygit() end, { desc = "Lazygit (cwd)" })
 end
 
 map("n", "<leader>fl", "<Cmd>lua Snacks.picker.git_log_file()<CR>", { desc = "File Logs" })
@@ -77,24 +75,3 @@ vim.api.nvim_create_autocmd("LspAttach", {
         map("n", "<leader>cr", vim.lsp.buf.rename, { desc = "Code Rename" })
     end,
 })
--- --     callback = function()
--- --         map("n", "gd", "<Cmd>lua Snacks.picker.lsp_definitions()<CR>", { desc = "Goto Definition" })
--- --         map("n", "gD", "<Cmd>lua Snacks.picker.lsp_declarations()<CR>", { desc = "Goto Declaration" })
--- --         map("n", "gr", "<Cmd>lua Snacks.picker.lsp_references()<CR>", { nowait = true, desc = "References" })
--- --         map("n", "gI", "<Cmd>lua Snacks.picker.lsp_implementations()<CR>", { desc = "Goto Implementation" })
--- --         map("n", "gy", "<Cmd>lua Snacks.picker.lsp_type_definitions()<CR>", { desc = "Goto T[y]pe Definition" })
--- --         map("n", "<leader>ss", "<Cmd>lua Snacks.picker.lsp_symbols()<CR>", { desc = "LSP Symbols" })
--- --         map("n", "<leader>sS", "<Cmd>lua Snacks.picker.lsp_workspace_symbols()<CR>", { desc = "LSP Workspace Symbols" })
--- --         map("n", "<leader>cr", vim.lsp.buf.rename, { desc = "Code Rename" })
--- --
--- --         -- wk.add({ "gd", function() Snacks.picker.lsp_definitions() end, desc = "Goto Definition" })
--- --         -- wk.add({ "gD", function() Snacks.picker.lsp_declarations() end, desc = "Goto Declaration" })
--- --         -- wk.add({ "gr", function() Snacks.picker.lsp_references() end, nowait = true, desc = "References" })
--- --         -- wk.add({ "gI", function() Snacks.picker.lsp_implementations() end, desc = "Goto Implementation" })
--- --         -- wk.add({ "gy", function() Snacks.picker.lsp_type_definitions() end, desc = "Goto T[y]pe Definition" })
--- --         -- wk.add({ "<leader>ss", function() Snacks.picker.lsp_symbols() end, desc = "LSP Symbols" })
--- --         -- wk.add({ "<leader>sS", function() Snacks.picker.lsp_workspace_symbols() end, desc = "LSP Workspace Symbols" })
--- --         -- wk.add({ "<leader>cr", vim.lsp.buf.rename, desc = "Code Rename" })
--- --     end,
--- -- })
--- -
