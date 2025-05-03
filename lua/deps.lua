a8debd6466c10b96bb9c2d8ed5f08b51c0bfae20
Chuan Miao <chuan137@gmail.com>
2025-05-03 20:41:32 +0200

    <C-L>: jump to end of current treesitter node

--- lua/deps.lua
+++ lua/deps.lua
@@ -147,6 +147,15 @@ else
             ensure_installed = { "lua", "vimdoc", "go", "python" },
             highlight = { enable = true },
         })
+        -- use <C-L> in insert mode to jump to end of current treesitter node
+        -- https://github.com/santhosh-tekuri/dotfiles/blob/master/nvim/lua/insjump.lua
+        vim.keymap.set("i", "<C-L>", function()
+            local node = vim.treesitter.get_node()
+            if node ~= nil then
+                local row, col = node:end_()
+                pcall(vim.api.nvim_win_set_cursor, 0, { row + 1, col })
+            end
+        end, { desc = "insjump" })
     end)
 
     later(require("plugins.snacks").setup)
