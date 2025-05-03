aa219a73a3817cf6bcf4aa97a902c0a40867b186
Chuan Miao <chuan137@gmail.com>
2025-05-03 20:05:38 +0200

    upate keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,20 +1,27 @@
 local map = vim.keymap.set
 
+map("n", "Q", "@q")
 map("n", "n", "nzzzv")
 map("n", "N", "Nzzzv")
 
 map({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
-map("v", "J", ":m '>+1<CR>gv=gv")
-map("v", "K", ":m '<-2<CR>gv=gv")
-map("x", "<leader>p", [["_dP]]) -- paste WON'T copy
 map("n", "<leader>w", "<C-w>")
 map("n", "<leader><Tab>", "<cmd>bnext<cr>")
 
 map("n", "<leader>rp", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>") -- Replace all instance of current word in file
 map("v", "<leader>rp", ":s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>") -- Replace all instance of current word in file
 
+-- move current line up/down
+map("n", "<M-j>", ":m .+1<CR>==")
+map("n", "<M-k>", ":m .-2<CR>==")
+
+-- move selected lines up/down
+map("v", "J", ":m '>+1<CR>gv=gv")
+map("v", "K", ":m '<-2<CR>gv=gv")
+
+-- copy paste
+map("x", "<leader>p", [["_dP]]) -- paste WON'T copy
 map("n", "<leader>up", "<cmd>set paste!<cr>", { desc = "Toggle Paste" })
-map("n", "Q", "@q")
 
 -- diagnostic
 local diagnostic_goto = function(next, severity)
@@ -25,8 +32,6 @@ local diagnostic_goto = function(next, severity)
     end
 end
 
--- stylua: ignore start
-map("n", "<leader>cd", vim.diagnostic.open_float, { desc = "Line Diagnostics" })
 map("n", "]d", diagnostic_goto(true), { desc = "Next Diagnostic" })
 map("n", "[d", diagnostic_goto(false), { desc = "Prev Diagnostic" })
 map("n", "]e", diagnostic_goto(true, "ERROR"), { desc = "Next Error" })
@@ -34,3 +39,5 @@ map("n", "[e", diagnostic_goto(false, "ERROR"), { desc = "Prev Error" })
 map("n", "]w", diagnostic_goto(true, "WARN"), { desc = "Next Warning" })
 map("n", "[w", diagnostic_goto(false, "WARN"), { desc = "Prev Warning" })
 
+-- stylua: ignore start
+map("n", "<leader>cd", vim.diagnostic.open_float, { desc = "Line Diagnostics" })
