503ea9ce7521298818c6ad2a723e0b7bc357d50e
Chuan Miao <chuan137@gmail.com>
2025-05-31 13:26:03 +0200

    update snack keymaps

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -6,16 +6,14 @@ return function()
         picker = { enabled = true },
         quickfile = { enabled = true },
         scope = { enabled = true },
-        statuscolumn = { enabled = true },
         words = { enabled = true },
+        -- statuscolumn = { enabled = true },
         -- notifier = { enabled = true },
         -- bigfile = { enabled = true },
         -- dashboard = { enabled = true },
         -- explorer = { enabled = true },
         -- scroll = { enabled = true },
     })
-
-    local map = vim.keymap.set
 
     -- toggle options
     Snacks.toggle.option("spell", { name = "Spelling" }):map("<leader>us")
@@ -41,37 +39,40 @@ return function()
         Snacks.toggle.inlay_hints():map("<leader>uh")
     end
 
-    -- lazygit
+    -- key maps
+    local map = vim.keymap.set
+
     if vim.fn.executable("lazygit") == 1 then
-        map("n", "<leader>gg", function()
-            Snacks.lazygit()
-        end, { desc = "Lazygit (cwd)" })
+        -- stylua: ignore start
+        map("n", "<leader>gg", function() Snacks.lazygit() end, { desc = "Lazygit (cwd)" })
     end
 
-    map("n", "<leader>gb", "<Cmd>lua Snacks.picker.git_log_line()<CR>", { desc = "Git Log Line" })
-    map("n", "<leader>gl", "<Cmd>lua Snacks.picker.git_log_file()<CR>", { desc = "Git File Logs" })
-    map("n", "<leader>gL", "<Cmd>lua Snacks.picker.git_log()<CR>", { desc = "Git Log" })
-    map({ "n", "x" }, "<leader>go", "<Cmd>lua Snacks.gitbrowse()<CR>", { desc = "Git Browse (open)" })
-    map({ "n", "x" }, "<leader>gY", function()
+    local yank_git_url = function()
         Snacks.gitbrowse({
             open = function(url)
                 vim.fn.setreg("+", url)
             end,
             notify = false,
         })
-    end, { desc = "Git Browse (copy)" })
+    end
 
-    map("n", "<leader>,", "<Cmd>lua Snacks.picker.buffers()<CR>", { desc = "Buffers" })
+    map("n", "<leader>gb", "<Cmd>lua Snacks.picker.git_log_line()<CR>", { desc = "Git Log Line" })
+    map("n", "<leader>gl", "<Cmd>lua Snacks.picker.git_log_file()<CR>", { desc = "Git File Logs" })
+    map("n", "<leader>gL", "<Cmd>lua Snacks.picker.git_log()<CR>", { desc = "Git Log" })
+    map({ "n", "x" }, "<leader>go", "<Cmd>lua Snacks.gitbrowse()<CR>", { desc = "Git Browse (open)" })
+    map({ "n", "x" }, "<leader>gY", yank_git_url, { desc = "Git Browse (copy)" })
+
     map("n", "<leader>/", "<Cmd>lua Snacks.picker.grep()<CR>", { desc = "Grep" })
     map("n", "<leader><cr>", "<Cmd>lua Snacks.picker.resume()<CR>", { desc = "Snacks Resume" })
     map("n", "<leader><space>", "<Cmd>lua Snacks.picker.git_files()<CR>", { desc = "Git Files" })
+    map("n", "<leader>fb", "<Cmd>lua Snacks.picker.buffers()<CR>", { desc = "Buffers" })
     map("n", "<leader>ff", "<Cmd>lua Snacks.picker.files()<CR>", { desc = "Files" })
-    map("n", "<leader>fw", "<Cmd>lua Snacks.picker.grep_word()<CR>", { desc = "Grep Word" })
-    map("n", "<leader>fr", "<Cmd>lua Snacks.picker.recent()<CR>", { desc = "Recent file" })
-    map("n", "<leader>fs", "<Cmd>lua Snacks.picker.grep_buffers()<CR>", { desc = "Grep Buffers" })
+    map("n", "<leader>fo", "<Cmd>lua Snacks.picker.recent()<CR>", { desc = "Recent file" })
+    map("n", "<leader>fv", "<Cmd>lua Snacks.picker.grep_word()<CR>", { desc = "Grep Word" })
+    map("n", "<leader>fx", "<Cmd>lua Snacks.picker.grep_buffers()<CR>", { desc = "Grep Buffers" })
 
     -- stylua: ignore start
     map("n", "<C-l>", function() Snacks.words.jump(1, true) end, { desc = "Jump to next word" })
     map("n", "<C-h>", function() Snacks.words.jump(-1, true) end, { desc = "Jump to next word" })
-        -- stylua: ignore end
+    -- stylua: ignore end
 end
