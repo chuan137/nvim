ec366260ff9d6e0db46b86b6cd06df8fccb30a3a
Chuan Miao <chuan137@gmail.com>
2025-07-26 11:17:20 +0200

    options for markdown file

---
+++ after/ftplugin/markdown.lua
@@ -0,0 +1,10 @@
+
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.wrap = true
+
+vim.opt_local.spell = true
+vim.opt_local.linebreak = true
+vim.opt_local.showbreak = "  ↪ "
+
