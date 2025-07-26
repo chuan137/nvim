6f59ac272d5f4ebdc2af2315abfb96226f20c668
Chuan Miao <chuan137@gmail.com>
2025-07-26 11:34:48 +0200

    format

--- init.lua
+++ init.lua
@@ -2,7 +2,7 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 vim.g.lazyloading = true
 
-local eval =require("utils").eval
+local eval = require("utils").eval
 vim.g.minimal = eval(os.getenv("NVIM_MINIMAL"))
 
 require("deps")
