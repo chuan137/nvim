82a262a9eda306bfe61eaafa263a80c15d769ef6
Chuan Miao <chuan137@gmail.com>
2026-01-02 12:13:43 +0100

    feat: enhance markdown plugin loading and configuration

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -9,32 +9,66 @@
 -- }
 
 return function()
+    -- Create autocommand to lazy load markdown plugin only for markdown filetypes
+    local markdown_loaded = false
+    
+    local function load_markdown_plugin()
+        if markdown_loaded then
+            return
+        end
+        markdown_loaded = true
 
+        -- =====================================================================
+        -- Configure render-markdown.nvim
+        -- =====================================================================
+        MiniDeps.add({
+            source = "MeanderingProgrammer/render-markdown.nvim",
+            depends = { "nvim-treesitter/nvim-treesitter", "nvim-mini/mini.nvim" },
+        })
 
-    MiniDeps.add({
-        source = "MeanderingProgrammer/render-markdown.nvim",
-        depends = { "nvim-treesitter/nvim-treesitter", "nvim-mini/mini.nvim" },
+        require("render-markdown").setup({
+            code = {
+                sign = false,
+                width = "block",
+                right_pad = 1,
+            },
+            heading = {
+                sign = false,
+                icons = {},
+            },
+            checkbox = {
+                enabled = false,
+            },
+        })
+
+        -- if Snacks is installed, add markdown rendering to Snacks preview
+        Snacks.toggle({
+            name = "Render Markdown",
+            get = require("render-markdown").get,
+            set = require("render-markdown").set,
+        }):map("<leader>um")
+        
+        -- =====================================================================
+        -- Configure markdown-preview.nvim
+        -- =====================================================================
+        -- vim.g.mkdp_filetypes = { "markdown" }
+
+        -- MiniDeps.add({
+        --     source = "iamcco/markdown-preview.nvim",
+        --     hooks = {
+        --         post_checkout = function(path, _, _)
+        --             vim.fn.system({ "npm", "install" }, path .. "/app")
+        --         end,
+        --     },
+        -- })
+    end
+
+    -- Set up autocommand for markdown filetype
+    vim.api.nvim_create_autocmd("FileType", {
+        pattern = "markdown",
+        callback = function()
+            load_markdown_plugin()
+        end,
+        desc = "Load markdown plugin for markdown files",
     })
-
-    require("render-markdown").setup({
-        code = {
-            sign = false,
-            width = "block",
-            right_pad = 1,
-        },
-        heading = {
-            sign = false,
-            icons = {},
-        },
-        checkbox = {
-            enabled = false,
-        },
-    })
-
-    -- if Snacks is installed, add markdown rendering to Snacks preview
-    Snacks.toggle({
-        name = "Render Markdown",
-        get = require("render-markdown").get,
-        set = require("render-markdown").set,
-    }):map("<leader>um")
 end
