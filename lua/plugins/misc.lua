9096fb94769fe394499a4e968e272e0ecb8ca30b
Chuan Miao <chuan137@gmail.com>
2026-02-09 09:53:43 +0100

    move prev-buffer to misc.lua

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -1,4 +1,8 @@
 return function()
     MiniDeps.add({ source = "dstein64/vim-startuptime" })
     MiniDeps.add({ source = "google/vim-jsonnet" })
+
+    -- previous-buffer.nvim
+    MiniDeps.add({ source = "kj-1809/previous-buffer.nvim" })
+    vim.keymap.set("n", "<leader>@", ":PreviousBuffer<CR>")
 end
--- lua/plugins/prev-buffer.lua
+++
@@ -1,6 +0,0 @@
-return function()
-    MiniDeps.add({
-        source = "kj-1809/previous-buffer.nvim",
-    })
-    vim.keymap.set("n", "<leader>]", ":PreviousBuffer<CR>", { silent = true })
-end
